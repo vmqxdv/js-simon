a7f8ef0d904a13c3327a0cba84a970f81c0a51ee
vmqxdv <vquintilio@gmail.com>
2025-03-06 14:07:55 +0100

    feat: Dichiarate costanti elementi

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui
+l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine
+che preferisce.
+
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali 
+dei numeri da indovinare sono stati individuati.
+
+*/
+
+const countDown = document.getElementById('countdown');
+const numberList = document.getElementById('numbers-list');
+const inputGroup = document.querySelectorAll('input');
+
